8e5e9a2877f1170f99a7c655eef57d07f24833ad
HeatblasterAA <amandeep.official60@gmail.com>
2026-06-19 12:21:50 +0530

    Spring project initialized

---
+++ src/test/java/com/example/_Social/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example._Social.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
